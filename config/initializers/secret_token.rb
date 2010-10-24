1bf47529a3643196c4020d060a0c25578c06a671
Halan Pinheiro <halan.pinheiro@gmail.com>
2010-10-24 20:18:57 -0300

    Commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redpanel::Application.config.secret_token = '3794a0868dca9766eafd0ec01cf14a2be474efc7ccd1a0a8ce14f5a1491706d6d01880e2b68b0cd907a937bc0f7b2a82c42406ef23d0b3bcdba50080bc6fba3b'
